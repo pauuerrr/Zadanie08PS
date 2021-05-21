51428560448eb56df176fc9e0e8b85c51abf8593
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-21 23:58:49 +0200

    Dodanie pola _config

--- Zadanie08PS/Pages/Index.cshtml.cs
+++ Zadanie08PS/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace Zadanie08PS.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _config;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
